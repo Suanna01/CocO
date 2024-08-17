588e8ebd0faea4a295605298f27f5c5f51b5e2e3
Suanna01 <yangnony01@gmail.com>
2024-08-17 16:30:42 +0900

    set: initial setting

---
+++ src/main/java/com/example/CocO/CocOApplication.java
@@ -0,0 +1,13 @@
+package com.example.CocO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocOApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocOApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CocO/CocOApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CocO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocOApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
