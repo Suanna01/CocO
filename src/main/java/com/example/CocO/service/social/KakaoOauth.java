b56556186bddb07ff2f2421854c8a98762af24e4
Suanna01 <yangnony01@gmail.com>
2024-09-10 18:43:26 +0900

    #2 feat: kakao social login

--- src/main/java/com/example/CocO/service/social/KakaoOauth.java
+++ src/main/java/com/example/CocO/service/social/KakaoOauth.java
@@ -1,16 +1,119 @@
 package com.example.CocO.service.social;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Slf4j
 @Component
+@RequiredArgsConstructor
 public class KakaoOauth implements SocialOauth {
+
+    @Value("${sns.kakao.url}")
+    private String KAKAO_SNS_BASE_URL;
+    @Value("${sns.kakao.client.id}")
+    private String KAKAO_SNS_CLIENT_ID;
+    @Value("${sns.kakao.callback.url}")
+    private String KAKAO_SNS_CALLBACK_URL;
+    @Value("${sns.kakao.client.secret}")
+    private String KAKAO_SNS_CLIENT_SECRET;
+    @Value("${sns.kakao.token.url}")
+    private String KAKAO_SNS_TOKEN_BASE_URL;
+
     @Override
     public String getOauthRedirectURL() {
-        return "";
+        Map<String, Object> params = new HashMap<>();
+        params.put("response_type", "code");
+        params.put("client_id", KAKAO_SNS_CLIENT_ID);
+        params.put("redirect_uri", KAKAO_SNS_CALLBACK_URL);
+
+        String parameterString = params.entrySet().stream()
+                .map(x -> x.getKey() + "=" + x.getValue())
+                .collect(Collectors.joining("&"));
+
+        return KAKAO_SNS_BASE_URL + "?" + parameterString;
     }
 
     @Override
     public String requestAccessToken(String code) {
-        return null;
+        RestTemplate restTemplate = new RestTemplate();
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("code", code);
+        params.put("client_id", KAKAO_SNS_CLIENT_ID);
+        params.put("client_secret", KAKAO_SNS_CLIENT_SECRET);
+        params.put("redirect_uri", KAKAO_SNS_CALLBACK_URL);
+        params.put("grant_type", "authorization_code");
+
+        ResponseEntity<String> responseEntity =
+                restTemplate.postForEntity(KAKAO_SNS_TOKEN_BASE_URL, params, String.class);
+
+//        if (responseEntity.getStatusCode() == HttpStatus.OK) {
+//            return responseEntity.getBody();
+//        }
+//        return "카카오 로그인 요청 처리 실패";
+        if (responseEntity.getStatusCode() == HttpStatus.OK) {
+            log.info("카카오 토큰 응답: {}", responseEntity.getBody());
+            return responseEntity.getBody();
+        } else {
+            log.error("카카오 로그인 요청 실패: {}", responseEntity.getBody());
+        }
+        return "카카오 로그인 요청 처리 실패";
+    }
+
+    public String requestAccessTokenUsingURL(String code) {
+        try {
+            URL url = new URL(KAKAO_SNS_TOKEN_BASE_URL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            conn.setDoOutput(true);
+
+            Map<String, Object> params = new HashMap<>();
+            params.put("code", code);
+            params.put("client_id", KAKAO_SNS_CLIENT_ID);
+            params.put("client_secret", KAKAO_SNS_CLIENT_SECRET);
+            params.put("redirect_uri", KAKAO_SNS_CALLBACK_URL);
+            params.put("grant_type", "authorization_code");
+
+            String parameterString = params.entrySet().stream()
+                    .map(x -> x.getKey() + "=" + x.getValue())
+                    .collect(Collectors.joining("&"));
+
+            BufferedOutputStream bous = new BufferedOutputStream(conn.getOutputStream());
+            bous.write(parameterString.getBytes());
+            bous.flush();
+            bous.close();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+            StringBuilder sb = new StringBuilder();
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+
+            if (conn.getResponseCode() == 200) {
+                return sb.toString();
+            }
+            return "카카오 로그인 요청 처리 실패";
+        } catch (IOException e) {
+            throw new IllegalArgumentException("알 수 없는 카카오 로그인 Access Token 요청 URL 입니다 :: " + KAKAO_SNS_TOKEN_BASE_URL);
+        }
     }
 }
