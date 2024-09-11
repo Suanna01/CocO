089ab270a377fdcb1e72b5c6e440149f7e0b5ab7
Suanna01 <yangnony01@gmail.com>
2024-09-11 16:58:13 +0900

    #2 feat: naver social login

--- src/main/java/com/example/CocO/service/social/NaverOauth.java
+++ src/main/java/com/example/CocO/service/social/NaverOauth.java
@@ -1,16 +1,62 @@
 package com.example.CocO.service.social;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Component
+@RequiredArgsConstructor
 public class NaverOauth implements SocialOauth {
+    @Value("${sns.naver.url}")
+    private String NAVER_SNS_BASE_URL;
+    @Value("${sns.naver.client.id}")
+    private String NAVER_SNS_CLIENT_ID;
+    @Value("${sns.naver.callback.url}")
+    private String NAVER_SNS_CALLBACK_URL;
+    @Value("${sns.naver.client.secret}")
+    private String NAVER_SNS_CLIENT_SECRET;
+    @Value("${sns.naver.token.url}")
+    private String NAVER_SNS_TOKEN_BASE_URL;
+
     @Override
     public String getOauthRedirectURL() {
-        return "";
+        Map<String, Object> params = new HashMap<>();
+        params.put("response_type", "code");
+        params.put("client_id", NAVER_SNS_CLIENT_ID);
+        params.put("redirect_uri", NAVER_SNS_CALLBACK_URL);
+        params.put("state", "random_state_value"); // CSRF 방지를 위한 state 파라미터 추가
+
+        String parameterString = params.entrySet().stream()
+                .map(x -> x.getKey() + "=" + x.getValue())
+                .collect(Collectors.joining("&"));
+
+        return NAVER_SNS_BASE_URL + "?" + parameterString;
     }
 
     @Override
     public String requestAccessToken(String code) {
-        return null;
+        RestTemplate restTemplate = new RestTemplate();
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("code", code);
+        params.put("client_id", NAVER_SNS_CLIENT_ID);
+        params.put("client_secret", NAVER_SNS_CLIENT_SECRET);
+        params.put("redirect_uri", NAVER_SNS_CALLBACK_URL);
+        params.put("grant_type", "authorization_code");
+        params.put("state", "random_state_value");
+
+        ResponseEntity<String> responseEntity = restTemplate.postForEntity(NAVER_SNS_TOKEN_BASE_URL, params, String.class);
+
+        if (responseEntity.getStatusCode() == HttpStatus.OK) {
+            return responseEntity.getBody();
+        }
+        return "네이버 로그인 요청 처리 실패";
     }
 }
