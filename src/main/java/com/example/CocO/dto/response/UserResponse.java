c0dd539901d6ac50dd42192e26c3acfa61c49302
Suanna01 <yangnony01@gmail.com>
2024-12-20 17:05:14 +0900

    #2 fix: modify dto

--- src/main/java/com/example/CocO/dto/response/UserResponse.java
+++ src/main/java/com/example/CocO/dto/response/UserResponse.java
@@ -1,5 +1,10 @@
 package com.example.CocO.dto.response;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserResponse {
     private String name;
     private String accessToken;
@@ -8,31 +13,6 @@ public class UserResponse {
     public UserResponse(String name, String accessToken, String provider) {
         this.name = name;
         this.accessToken = accessToken;
-        this.provider = provider;
-    }
-
-    // Getter, Setter 추가
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAccessToken() {
-        return accessToken;
-    }
-
-    public void setAccessToken(String accessToken) {
-        this.accessToken = accessToken;
-    }
-
-    public String getProvider() {
-        return provider;
-    }
-
-    public void setProvider(String provider) {
         this.provider = provider;
     }
 }
